451ef6260d53e0ed33c03c4037421d5bb5e4ca71
админ <dimansen15@inbox.ru>
2020-12-01 13:01:43 +0300

    тестовое добавление изменения

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        тестовое изменение
+        String[] names;
+    names = new String[10];
+    names[0]= "abc";
+    String[] names2 = {"cde", "fgh"};
+    System.out.println(names[0]);
+    System.out.println(names2[0]);
+
+    names2=names;
+    System.out.println(names[0]);
+    System.out.println(names2[0]);
+
+    names2[2]="klm";
+    System.out.println(names[2]);
+    System.out.println(names2[2]);
+
+    int i=10;
+    //Integer j = new Integer(value) 11)
+
     }
 }
