d4a9282e6621221697b112ca2faa355a3883d505
dimansen68 <dimansen15@inbox.ru>
2020-12-02 23:25:12 +0300

    home work 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,24 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        String[] names;
-    names = new String[10];
-    names[0]= "abc";
-    String[] names2 = {"cde", "fgh"};
-    System.out.println(names[0]);
-    System.out.println(names2[0]);
 
-    names2=names;
-    System.out.println(names[0]);
-    System.out.println(names2[0]);
-
-    names2[2]="klm";
-    System.out.println(names[2]);
-    System.out.println(names2[2]);
-
-    int i=10;
-    //Integer j = new Integer(value) 11)
+     double price = 54.79;
+     int volume = 70;
+     double cost = price * volume;
+     System.out.println(cost);
 
     }
 }
